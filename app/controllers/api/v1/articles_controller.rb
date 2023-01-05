3afac3317bf193cb9d3f796689bb8c4dc6a3206d
Peter Gichia <petergichia35@gmail.com>
2023-01-05 11:50:49 +0300

    Feature: Add endpoints to create and delete articles, also create a resusable menthod to whitelist articles params

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,12 +4,36 @@ module Api
     class ArticlesController < ApplicationController
       def index
         articles = Article.order('created_at DESC')
-        render json: { status: 'SUCCESS', data: articles }, status: :ok
+        render json: { status: 'SUCCESS', message: 'Articles loaded', data: articles }, status: :ok
       end
 
       def show
         article = Article.find(params[:id])
-        render json: { status: 'SUCCESS', data: article }, status: :ok
+        render json: { status: 'SUCCESS', message: 'Article loaded', data: article }, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+        if article.save
+          render json: { status: 'SUCCESS', message: 'Article saved', data: article }, status: :created
+        else
+          render json: { status: 'ERROR', message: 'Article not saved!', data: article.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+
+        render json: { status: 'SUCCESS', message: 'Article deleted' }, status: :ok
+      end
+
+      private
+
+      # Only allow a list of trusted parameters through.
+      def article_params
+        params.require(:article).permit(:title, :body)
       end
     end
   end
