349f13f43ba99e8939a20a8f0f9827ed9b96aeaf
Peter Gichia <petergichia35@gmail.com>
2023-01-05 10:15:53 +0300

    Feature: Create migration file for the articles table

---
+++ db/migrate/20230105065924_create_articles.rb
@@ -0,0 +1,11 @@
+# Migration for creating articles table
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
